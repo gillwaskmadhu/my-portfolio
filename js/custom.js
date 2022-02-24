274ebfec599f404400e4c518f515ba9e9a39f396
gillwaskmadhu <50201242+gillwaskmadhu@users.noreply.github.com>
2022-02-24 14:31:28 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
+
+
+// slick slider
+
+$('.wrapper').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+});
+
+
+//test
+
